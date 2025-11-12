ab7a7a7781c00946e660f0cdaf90a07d6a3010c1
ezequielmenor <ezequielmenor@gmail.com>
2025-11-12 23:00:48 +0100

    feat: Implementar interactividad con ComboBox para selección de ejercicio

--- src/main/java/com/ezequiel/logic/CalcularMetricasService.java
+++ src/main/java/com/ezequiel/logic/CalcularMetricasService.java
@@ -3,11 +3,16 @@ package com.ezequiel.logic;
 import com.ezequiel.model.Entrenamiento;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CalcularMetricasService {
 
     public double encontrarPesoMaximo(List<Entrenamiento> entrenamientoList, String nombreEjercicio) {
         return entrenamientoList.stream().filter(e -> e.getExerciseTitle().equalsIgnoreCase(nombreEjercicio)).mapToDouble(Entrenamiento::getWeightKg).max().orElse(0.0);
-
     }
+
+    public List<String> getEjerciciosUnicos(List<Entrenamiento> entrenamientos) {
+        return entrenamientos.stream().map(Entrenamiento::getExerciseTitle).distinct().collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/ezequiel/ui/MainController.java
+++ src/main/java/com/ezequiel/ui/MainController.java
@@ -5,7 +5,10 @@ import com.ezequiel.model.Entrenamiento;
 import com.ezequiel.repository.CsvEntrenamientoRepositorio;
 import com.ezequiel.repository.EntrenamientoRepositorio;
 import com.ezequiel.repository.JdbcEntreneRepo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 import java.util.List;
@@ -14,20 +17,35 @@ public class MainController {
 
     @FXML
     private Label lblResultado;
+    @FXML
+    private ComboBox<String> cmbEjercicios;
+
+    List<Entrenamiento> entrenamientos;
+    CalcularMetricasService service;
 
     public void initialize() {
         EntrenamientoRepositorio repo = new JdbcEntreneRepo();
-        List<Entrenamiento> entrenamientos = repo.obtenerTodos();
+        this.entrenamientos = repo.obtenerTodos();
+        this.service = new CalcularMetricasService();
 
+        List<String> ejerciciosUnicos = service.getEjerciciosUnicos(entrenamientos);
+        ObservableList<String> observableList = FXCollections.observableList(ejerciciosUnicos);
+        cmbEjercicios.setItems(observableList);
 
         if (!entrenamientos.isEmpty()) {
-            CalcularMetricasService service = new CalcularMetricasService();
-            String ejercicioBuscado = "Press de Banca (Barra)";
-            double maxPeso = service.encontrarPesoMaximo(entrenamientos, ejercicioBuscado);
-
-            lblResultado.setText("Peso maximo de " + ejercicioBuscado + ": " + maxPeso);
+            cmbEjercicios.setValue(observableList.get(0));
+            actualizarMetricas();
         } else {
             lblResultado.setText("No se encontro ese entrenamiento");
+        }
+    }
+
+    public void actualizarMetricas(){
+        String ejercicioSeleccionado = cmbEjercicios.getValue();
+
+        if (ejercicioSeleccionado != null && this.entrenamientos != null) {
+            double maxPeso = this.service.encontrarPesoMaximo(this.entrenamientos, ejercicioSeleccionado);
+            lblResultado.setText("Peso máximo de " + ejercicioSeleccionado + " : " + maxPeso + " kg");
         }
     }
 }
